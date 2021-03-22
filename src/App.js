3780f5af44382c01c3377dd622d71d0297629e1e
Muhammad Muneeb Waseem <muneebwaseem78@gmail.com>
2021-03-22 21:07:25 +0500

    pushing input, submit and show msg functionalities

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React, { useState } from 'react'; 
 function App() {
+  const [input, setInput] = useState('');
+
+  
+  //LIST OF MSGS BY STORING THEM IN STATE
+  
+  const [messages, setMessages] = useState(['hi', 'good']); //MSGS WILL OBVIOUSLY IN ARRAY
+  console.log(messages)
+  //FUNCTION FOR SENDING MSG BY CLICKING ON BUTTON
+ 
+  const sendMessage = (e) => {
+    //Storing msg into list
+    e.preventDefault();//Stop form from refreshing
+    
+    setMessages([...messages, input]) //placed that input msg in a list beside all (...) msgs   
+    setInput('');//After that make input field empty again
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hey yo! What's going on?</h1>
+
+      <form>
+        <input value={input} onChange={(e)=>setInput(e.target.value)}></input>
+        <button type='submit' onClick={sendMessage}>Send Message</button>
+      </form>
+      {/*onclicking the above button, form tries to refresh, event.preventDefault() will stop it from doing this in sendMessage function}
+
+      {/*messages themselves*/}
+      {/*Displaying msgs}*/}
+
+      {
+        messages.map((message) => 
+          <p key={message}>{message}</p>
+        )
+      }
+
     </div>
   );
 }
