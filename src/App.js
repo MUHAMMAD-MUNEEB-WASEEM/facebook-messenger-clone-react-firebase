fe6821b713382ed354412def77f20f176784c584
Muhammad Muneeb Waseem <muneebwaseem78@gmail.com>
2021-03-23 08:24:05 +0500

    upadated input functionality using material ui

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import React, { useState } from 'react'; 
+import { Button, FormControl, Input, InputLabel } from '@material-ui/core';
 function App() {
   const [input, setInput] = useState('');
 
@@ -25,8 +26,14 @@ function App() {
       <h1>Hey yo! What's going on?</h1>
 
       <form>
-        <input value={input} onChange={(e)=>setInput(e.target.value)}></input>
-        <button type='submit' onClick={sendMessage}>Send Message</button>
+
+      <FormControl>
+        <InputLabel >Enter a message...</InputLabel>
+        <Input value={input} onChange={(e)=>setInput(e.target.value)}/>
+        <Button disabled={!input} variant='contained' color='primary' type='submit' onClick={sendMessage}>Send Message</Button>
+
+      </FormControl>
+
       </form>
       {/*onclicking the above button, form tries to refresh, event.preventDefault() will stop it from doing this in sendMessage function}
 
